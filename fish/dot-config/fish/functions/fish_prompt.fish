1e08862e1064b8d824751d6c135e1a5960f2f701
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-03 04:51:28 -0800

    more fish prompt touchups

--- fish/dot-config/fish/functions/fish_prompt.fish
+++ fish/dot-config/fish/functions/fish_prompt.fish
@@ -7,7 +7,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l suffix ' $'
+    set -l suffix '><(((*>'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
@@ -27,5 +27,5 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -s -e (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (set_color magenta) (fish_vcs_prompt) " "$prompt_status (set_color bryellow --bold) $suffix "\n"
+    echo -s -e $prompt_status (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (set_color magenta) (fish_vcs_prompt) " " (set_color bryellow --bold) $suffix "\n"
 end
