3e5577ab7ded06f5623ec0d527a3d2e165ccbb25
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-03 21:18:30 -0800

    increased no-abbr limit for fish pwd

--- fish/dot-config/fish/functions/fish_prompt.fish
+++ fish/dot-config/fish/functions/fish_prompt.fish
@@ -77,5 +77,5 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -s -e $prompt_status (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) " " (set_color bryellow --bold) $suffix "\n"
+    echo -s -e $prompt_status (prompt_login)' ' (set_color $color_cwd) (prompt_pwd -D 7) $normal (fish_vcs_prompt) " " (set_color bryellow --bold) $suffix "\n"
 end
