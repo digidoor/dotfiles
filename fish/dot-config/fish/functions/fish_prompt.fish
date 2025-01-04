1b83d7401895cf7a8b74a3cdf13a63296d258493
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-03 18:02:12 -0800

    stowrc+ignore stuff, fishprompt touchups

--- fish/dot-config/fish/functions/fish_prompt.fish
+++ fish/dot-config/fish/functions/fish_prompt.fish
@@ -21,16 +21,23 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_git_prompt_showupstream informative
     set -g __fish_git_prompt_use_informative_chars 1
 
-#    set -q __fish_git_prompt_showdirtystate
-#    or set -g __fish_git_prompt_showdirtystate 1
-#    set -q __fish_git_prompt_showuntrackedfiles
-#    or set -g __fish_git_prompt_showuntrackedfiles 1
-#    set -q __fish_git_prompt_showstashstate
-#    or set -g __fish_git_prompt_showstashstate 1
-#    set -q __fish_git_prompt_showcolorhints
-#    or set -g __fish_git_prompt_showcolorhints 1
-#    set -q __fish_git_prompt_color_untrackedfiles
-#    or set -g __fish_git_prompt_color_untrackedfiles yellow
+    # Color doesn't work for the upstream indicators. maybe we can work
+    # on it later or something.
+#   set -q __fish_git_prompt_color_upstream_ahead
+#   or set -g __fish_git_prompt_color_upstream_ahead yellow
+#   set -q __fish_git_prompt_color_upstream_behind
+#   or set -g __fish_git_prompt_color_upstream_behind red
+
+    set -q __fish_git_prompt_showdirtystate
+    or set -g __fish_git_prompt_showdirtystate 1
+    set -q __fish_git_prompt_showuntrackedfiles
+    or set -g __fish_git_prompt_showuntrackedfiles 1
+    set -q __fish_git_prompt_showstashstate
+    or set -g __fish_git_prompt_showstashstate 1
+    set -q __fish_git_prompt_showcolorhints
+    or set -g __fish_git_prompt_showcolorhints 1
+    set -q __fish_git_prompt_color_untrackedfiles
+    or set -g __fish_git_prompt_color_untrackedfiles yellow
     set -q __fish_git_prompt_char_untrackedfiles
     or set -g __fish_git_prompt_char_untrackedfiles '%'
     set -q __fish_git_prompt_color_invalidstate
@@ -41,14 +48,14 @@ function fish_prompt --description 'Write out the prompt'
     or set -g __fish_git_prompt_color_dirtystate blue
     set -q __fish_git_prompt_char_dirtystate
     or set -g __fish_git_prompt_char_dirtystate '*'
+    set -q __fish_git_prompt_color_stagedstate
+    or set -g __fish_git_prompt_color_stagedstate yellow
     set -q __fish_git_prompt_char_stagedstate
     or set -g __fish_git_prompt_char_stagedstate '+'
     set -q __fish_git_prompt_color_cleanstate
     or set -g __fish_git_prompt_color_cleanstate green
     set -q __fish_git_prompt_char_cleanstate
     or set -g __fish_git_prompt_char_cleanstate '✓'
-    set -q __fish_git_prompt_color_stagedstate
-    or set -g __fish_git_prompt_color_stagedstate yellow
     set -q __fish_git_prompt_color_branch_dirty
     or set -g __fish_git_prompt_color_branch_dirty red
     set -q __fish_git_prompt_color_branch_staged
