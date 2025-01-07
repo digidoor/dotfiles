5bd577b7897d6ceb4263a32fc148b5cebfc530dc
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-07 05:07:31 -0800

    alternate way to set nvim plugin options

--- nvim/dot-config/nvim/lua/plugins/colors.lua
+++ nvim/dot-config/nvim/lua/plugins/colors.lua
@@ -12,15 +12,11 @@ return
     	"catgoose/nvim-colorizer.lua",
     	event = "BufReadPre",
     	opts = { -- set to setup table
+    		filetypes = {
+    			"css",
+    			"javascript",
+    			html = { mode = "foreground" },
+    		},
     	},
-    	config = function()
-    		require('colorizer').setup({
-    			filetypes = {
-    				"css",
-    				"javascript",
-    				html = { mode = "foreground" },
-    			},
-    		})
-    	end
 	},
 }
