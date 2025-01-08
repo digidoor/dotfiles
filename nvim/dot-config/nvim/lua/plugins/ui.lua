9277f19e0b5034dd24468cfafee5d54364df572c
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-07 16:11:22 -0800

    learned how to minimal install plugins with opts

--- nvim/dot-config/nvim/lua/plugins/ui.lua
+++ nvim/dot-config/nvim/lua/plugins/ui.lua
@@ -2,12 +2,18 @@ return
 {
 	{
 		'nvim-lualine/lualine.nvim',
-		enabled = true,
+		-- enabled = true, -- unnecessary since enabled is the default
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup()
-		end
+--		config = function()
+--			require('lualine').setup {
+--				options = {
+--					theme = 'tokyonight'
+--				}
+--			}
+--		end,
+		-- with this simple opts entry, the plugin is enabled with default opts
+		opts = {}, -- either we use this or we uncomment the config function
+
 	}
 }
-
 
