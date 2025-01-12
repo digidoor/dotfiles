feed0dc09e0897cde48a8e9d00d67f6a64ece758
digidoor <37601466+digidoor@users.noreply.github.com>
2025-01-12 13:46:30 -0800

    TODO color comments working

--- nvim/dot-config/nvim/lua/plugins/snacks.lua
+++ nvim/dot-config/nvim/lua/plugins/snacks.lua
@@ -5,6 +5,7 @@ return
 		priority = 1000,
 		lazy = false,
 		---@type snacks.Config
+		---
 		opts = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
@@ -18,11 +19,34 @@ return
 			scroll = { enabled = false },
 			statuscolumn = { enabled = false },
 			words = { enabled = false },
+			-- level = vim.log.levels.TRACE,
 			styles = {
 				notification = {
 					wo = { wrap = true } -- Wrap notifications
-				}
-			}
+				},
+				notification_history = {
+					border = "rounded",
+					zindex = 100,
+					width = 0.6,
+					height = 0.6,
+					minimal = false,
+					title = " Notification History ",
+					title_pos = "center",
+					ft = "markdown",
+					bo = { filetype = "snacks_notif_history", modifiable = false },
+					wo = { winhighlight = "Normal:SnacksNotifierHistory" },
+					keys = { q = "close" },
+				},
+			},
 		},
+		config = function(_, opts)
+			require("snacks").setup(opts)
+			-- additional config
+			vim.api.nvim_create_user_command('Messages', function()
+				require('snacks').notifier.show_history()
+			end, { nargs = '*' })
+			vim.cmd('cabbrev messages Messages')
+			vim.cmd('cabbrev m Messages')
+		end,
 	}
 }
